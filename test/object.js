3316786f0d4c95e4c65cfc4fdfeb6b0527a17fbf
hhh <1340545944@qq.com>
2022-10-05 10:46:43 +0800

    modify parameter type of `merge`

--- test/object.js
+++ test/object.js
@@ -14,11 +14,11 @@ exports.objectTests = {
         const bar = { a: -1, c: 2 };
         const baz = { d: 3 };
 
-        const result1 = HUtils.merge([foo, bar]);
+        const result1 = HUtils.merge(foo, bar);
         ctx.assert(result1 !== foo);
         ctx.assertDeepEqual(result1, { a: -1, b: 1, c: 2 });
 
-        const result2 = HUtils.merge([foo, baz]);
+        const result2 = HUtils.merge(foo, baz);
         ctx.assert(result2 !== foo);
         ctx.assertDeepEqual(result2, { a: 0, b: 1, d: 3 });
 
